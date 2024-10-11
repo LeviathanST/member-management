07feaf78e2e154667fb261539b88af8d4d34672b
LeviathanST <phamquochung170505@gmail.com>
2024-10-11 20:40:14 +0700

    update: clean main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,21 +1,17 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import models.SignUpData;
-import services.Auth;
-
 public class Main {
 	public static void main(String[] args) {
+		String db_user = "root";
+		String db_password = "admin";
+
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connection = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/member-management",
-					"root",
-					"admin");
-
-			SignUpData data = new SignUpData("hung", "hungdeptrai");
-
-			Auth.SignUp(connection, data);
+					db_user,
+					db_password);
 
 			System.out.println("Connection successfully!");
 
