76b912246bf7bb9c979a177e60adc2e71991262e
quochuybb <nqhsktt1@gmail.com>
2024-10-15 00:14:24 +0700

    refactor code: change name file and add feature crud

--- src/main/data/SignUpData.java
+++ src/main/data/SignUpData.java
@@ -1,22 +1,47 @@
 package data;
 
+import java.sql.Date;
+
 public class SignUpData {
 	private String username;
 	private String password;
-
+	private String email;
+	private boolean is_active;
+	private Date created_at;
+	private Date updated_at;
+	private int count_mistake;
 	public SignUpData(String username, String password) {
 		this.username = username;
 		this.password = password;
+		this.email = email;
+		this.is_active = is_active;
+		this.created_at = new Date(System.currentTimeMillis());
+		this.updated_at = new Date(System.currentTimeMillis());
+		this.count_mistake = count_mistake;
 	}
+
+	public String getEmail() {return this.email;}
+	public void setEmail(String email) {this.email = email;}
+
+	public boolean is_active() {return this.is_active;}
+	public void set_is_active(boolean is_active) {this.is_active = is_active;}
+
+	public Date getCreated_at() {return this.created_at;}
+
+	public Date getUpdated_at() {return this.updated_at;}
+	public void setUpdated_at(Date updated_at) {this.updated_at = updated_at;}
+
+	public int getCount_mistake() {return this.count_mistake;}
+	public void setCount_mistake(int count_mistake) {this.count_mistake = count_mistake;}
 
 	public String getUsername() {
 		return this.username;
 	}
+	public void setUserName(String username){this.username= username;}
 
 	public String getPassword() {
 		return this.password;
 	}
-
 	public void setPassword(String pwd) {
 		this.password = pwd;
 	}
--- src/main/models/UserAccount.java
+++ src/main/models/UserAccount.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -11,7 +12,7 @@ import exceptions.DataEmptyException;
 public class UserAccount {
 
 	public static void insert(Connection con, SignUpData data)
-			throws DataEmptyException, SQLException, SQLIntegrityConstraintViolationException {
+			throws DataEmptyException, SQLException {
 
 		String query = "INSERT INTO user_account (username, hashed_password) VALUES (?, ?)";
 
@@ -29,5 +30,63 @@ public class UserAccount {
 			throw new SQLException("Create new user is failed, no row is effected!");
 
 		System.out.println("Create new user is successfully!");
+	}
+	public static void edit(Connection con, SignUpData data)
+			throws DataEmptyException, SQLException {
+		Field[] fields = data.getClass().getDeclaredFields();
+		//Create query
+		StringBuilder query = new StringBuilder("UPDATE user_account SET ");
+		for (Field field : fields) {
+			if (field.getName().equals("password")) {
+				query.append("hashed_password=?, ");
+			}
+			else {
+				query.append(field.getName()).append(" = ?, ");
+			}
+
+		}
+		query.delete(query.length() - 2, query.length());
+		query.append(" WHERE username = ?");
+		if (data == null ) {
+			throw new DataEmptyException("Your username is empty");
+		}
+		try {
+
+			PreparedStatement stmt = con.prepareStatement(query.toString());
+			int index = 1;
+			for (Field field : fields) {
+				field.setAccessible(true);
+				stmt.setObject(index++, field.get(data));
+			}
+			stmt.setString(index++, data.getUsername());
+			int rowUpdated = stmt.executeUpdate();
+			if (rowUpdated == 0)
+				throw new SQLException("Edit user is failed, no row is effected!");
+			System.out.println("Edit user is successfully!");
+		} catch (SQLIntegrityConstraintViolationException e) {
+			System.err.println("Your username isn't existed!");
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+	public static void delete(Connection con, String data)
+			throws DataEmptyException, SQLException {
+		String query = "DELETE FROM user_account WHERE username = ?";
+		if (data == null ) {
+			throw new DataEmptyException("Your username is empty");
+		}
+
+		try {
+			PreparedStatement stmt = con.prepareStatement(query);
+			stmt.setString(1, data);
+			int rowEffected = stmt.executeUpdate();
+			if (rowEffected == 0)
+				throw new SQLException("Delete user is failed, no row is effected!");
+			System.out.println("Delete is successfully!");
+		} catch (SQLIntegrityConstraintViolationException e) {
+			System.err.println("Your username is existed!");
+		} catch (Exception e) {
+			System.err.println(e);
+		}
 	}
 }
