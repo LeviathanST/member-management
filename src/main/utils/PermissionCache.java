2ead81cb2dbc229b57681f57c4ab6cc0ceb1bf84
LeviathanST <phamquochung170505@gmail.com>
2024-10-15 03:07:19 +0700

    feat: role caching

---
+++ src/main/utils/PermissionCache.java
@@ -0,0 +1,42 @@
+package utils;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import contants.RoleType;
+import exceptions.CacheException;
+
+public class PermissionCache {
+	/// Type: Application, Crew, Guild
+	/// Name:
+	/// + Application: President, Vice President
+	/// + Guild: Media, HR, Technical, Plan
+	/// + Crew: AI, BE, FE, SECURITY, GAME
+	/// Name Permission:
+	/// EX: Write, Edit, Delete, ...
+
+	private static HashMap<RoleType, HashMap<String, HashSet<String>>> permissions = new HashMap<>();
+
+	public static void addPermission(RoleType type, String name, String namePermission) throws CacheException {
+		permissions.putIfAbsent(type, new HashMap<>());
+
+		permissions.get(type).putIfAbsent(name, new HashSet<String>());
+
+		boolean isAdded = permissions.get(type).get(name).add(namePermission);
+
+		if (!isAdded) {
+			throw new CacheException("This permission is already contained in cache!");
+		}
+	}
+
+	public static HashSet<String> contains(RoleType type, String name, String namePermission) {
+		if (permissions.containsKey(type) && permissions.get(type).containsKey(name)) {
+			return permissions.get(type).get(name);
+		}
+		return null;
+	}
+
+	public void clear() {
+		permissions.clear();
+	}
+}
