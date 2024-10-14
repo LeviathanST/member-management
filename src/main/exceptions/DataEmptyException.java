bd5624efc1017764e97c15842c0100da62e23b25
LeviathanST <phamquochung170505@gmail.com>
2024-10-14 18:41:47 +0700

    refactor: change name exceptions

--- src/main/exceptions/DataEmptyException.java
+++ src/main/exceptions/DataEmptyException.java
@@ -1,9 +1,9 @@
 package exceptions;
 
-public class ExceptionDataEmpty extends Exception {
+public class DataEmptyException extends Exception {
 	public String message;
 
-	public ExceptionDataEmpty(String message) {
+	public DataEmptyException(String message) {
 		this.message = message;
 		System.err.println("Exception Data Empty: " + message);
 		printStackTrace();
