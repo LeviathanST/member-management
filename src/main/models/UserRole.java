719aaa4f7cd5ba40e9ed8382adf034a96a46c247
LeviathanST <phamquochung170505@gmail.com>
2024-10-16 02:08:19 +0700

    add: get crew, guild, role id by account_id

--- src/main/models/UserRole.java
+++ src/main/models/UserRole.java
@@ -2,9 +2,29 @@ package models;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import exceptions.NotFoundException;
+
 public class UserRole {
+
+	public static int getIdByAccountId(Connection con, String account_id) throws SQLException, NotFoundException {
+		String query = """
+				SELECT id FROM user_role WHERE account_id = ?
+				""";
+
+		PreparedStatement stmt = con.prepareStatement(query);
+		stmt.setString(1, account_id);
+
+		ResultSet rs = stmt.executeQuery();
+
+		while (rs.next()) {
+			return rs.getInt("id");
+		}
+
+		throw new NotFoundException("This account not have any user_role in application!");
+	}
 
 	public static void insert(Connection con, String account_id, int role_id) throws SQLException {
 		String query = """
