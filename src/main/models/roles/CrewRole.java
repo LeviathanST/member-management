afbc132a7b3811b8ef5df4f85ccc64d9fc6372c7
TDVien05 <vientruongdoan@gmail.com>
2024-10-15 11:41:02 +0700

    Update : fix update and delete crew member

--- src/main/models/roles/CrewRole.java
+++ src/main/models/roles/CrewRole.java
@@ -76,19 +76,18 @@ public class CrewRole {
 			throw new SQLException("Insert failured");
 	}
 
-	public void updateCrewMember(Connection con, CrewData crewData, String newCrewRoleId) throws SQLException, NotFoundException{
+	public void updateCrewMember(Connection con, CrewData crewData, int newCrewRoleId) throws SQLException, NotFoundException{
 		String account_id = getAccountId(con, crewData);
-		int crew_role_id = getCrewRoleId(con, crewData);
 		String query = "UPDATE user_role SET crew_role_id = ? WHERE account_id = ?";
 		PreparedStatement stmt = con.prepareStatement(query);
-		stmt.setInt(1, crew_role_id);
+		stmt.setInt(1, newCrewRoleId);
 		stmt.setString(2, account_id);
 		int row = stmt.executeUpdate();
 		if(row == 0)
 			throw new SQLException("Update failured");
 	} 
 
-	public void deleteCrewMember(Connection con, CrewData crewData, String CrewRoleId) throws SQLException, NotFoundException{
+	public void deleteCrewMember(Connection con, CrewData crewData) throws SQLException, NotFoundException{
 		int crew_role_id = getCrewRoleId(con, crewData);
 		String query = "DELETE FROM user_role WHERE crew_role_id = ?";
 		PreparedStatement stmt = con.prepareStatement(query);
