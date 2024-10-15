dc079d4efb1f0b4f23864e40008faf1e0a697ee4
LeviathanST <phamquochung170505@gmail.com>
2024-10-16 01:18:50 +0700

    feat(models/roles): insert user role

--- src/main/models/UserRole.java
+++ src/main/models/UserRole.java
@@ -2,43 +2,22 @@ package models;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.management.relation.Role;
-
-import exceptions.NotFoundException;
-import models.roles.CrewRole;
-import models.roles.GuildRole;
-
 public class UserRole {
-	private Role role;
-	private CrewRole crew_role;
-	private GuildRole guild_role;
 
-	private Connection con;
+	public static void insert(Connection con, String account_id, int role_id) throws SQLException {
+		String query = """
+				INSERT INTO user_role (account_id, role_id)
+				VALUES (?, ?)
+				""";
 
-	private UserRole(Role role, CrewRole crew_role, GuildRole guild_role, Connection con) {
-		this.role = role;
-		this.crew_role = crew_role;
-		this.guild_role = guild_role;
-		this.con = con;
+		PreparedStatement stmt = con.prepareStatement(query);
+		stmt.setString(1, account_id);
+		stmt.setInt(2, role_id);
+
+		int rowEffected = stmt.executeUpdate();
+		if (rowEffected == 0)
+			throw new SQLException("Add role failed!");
 	}
-
-	// public static UserRole getByAccountId(Connection con, String account_id)
-	// throws NotFoundException, SQLException {
-	//
-	// String query = "SELECT role_id, crew_role_id, guild_role_id FROM user_role
-	// WHERE account_id = ?";
-	//
-	// PreparedStatement stmt = con.prepareStatement(query);
-	// stmt.setString(1, account_id);
-	// ResultSet rs = stmt.executeQuery();
-	//
-	// if (!rs.next()) {
-	// throw new NotFoundException("AccountID is not existed!");
-	// }
-	//
-	// return new UserRole();
-	// }
 }
