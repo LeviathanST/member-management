30ba4cfebab7b1581d6f64c7ee30fb41266167a2
LeviathanST <phamquochung170505@gmail.com>
2024-10-24 23:51:18 +0700

    update

---
+++ src/main/Database.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.sql.*;
+
+import config.DbConfig;
+import utils.EnvLoader;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+    public static Connection connection() throws IOException, SQLException, ClassNotFoundException {
+        DbConfig dbConfig = EnvLoader.load(DbConfig.class);
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String db_url = dbConfig.getDbUrl();
+        String db_password = dbConfig.getDbPassword();
+        Connection connection = DriverManager.getConnection(
+                db_url,
+                "root",
+                db_password);
+        System.out.println("Connection successfully!");
+        return connection;
+    }
+}
