5853bfd7fbc6b44064919228e07803e186f709ec
LeviathanST <phamquochung170505@gmail.com>
2025-02-14 23:24:29 +0700

    forgot to define import :(

--- src/main/controllers/AuthController.java
+++ src/main/controllers/AuthController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
