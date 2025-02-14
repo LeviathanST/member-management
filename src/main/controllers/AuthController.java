a32835bb57a2162c886d748d0e394a3a3d7c0852
Ngo Hoang Thai Duong <duongngo2209@gmail.com>
2025-02-15 00:34:20 +0700

    commit to pull

--- src/main/controllers/AuthController.java
+++ src/main/controllers/AuthController.java
@@ -24,6 +24,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLIntegrityConstraintViolationException;
 import services.AuthService;
 
 @WebServlet("/auth/*")
