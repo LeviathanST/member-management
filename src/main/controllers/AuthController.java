6454d082397c1534df60b05bc69265b951a8d04b
LeviathanST <phamquochung170505@gmail.com>
2025-02-14 15:39:17 +0700

    update(auth): convert controller to http servlet

--- src/main/controllers/AuthController.java
+++ src/main/controllers/AuthController.java
@@ -5,7 +5,13 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+
 import constants.ResponseStatus;
+import utils.HttpUtil;
 import dto.LoginDTO;
 import dto.ResponseDTO;
 import dto.SignUpDTO;
@@ -22,31 +28,31 @@ import services.AuthService;
 
 @WebServlet("/auth/*")
 public class AuthController extends HttpServlet {
+	private final String SIGNUP_VIEW = "view/signup.jsp";
+	private final String LOGIN_VIEW = "view/login.jsp";
+	private final String NOTFOUND_VIEW = "view/notfound.jsp";
+
+	private Gson gson = new Gson();
+	private Logger logger = LoggerFactory.getLogger(AuthController.class);
+	private String redirectPage;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		String route = (req.getPathInfo() != null) ? req.getPathInfo().substring(1) : "";
 		try {
 			switch (route) {
 				case "signup":
-					res.setContentType("text/html");
-					try (PrintWriter out = res.getWriter()) {
-						out.print("SignUp");
-					}
+					req.getRequestDispatcher(SIGNUP_VIEW);
 					break;
 				case "login":
-					res.setContentType("text/html");
-					try (PrintWriter out = res.getWriter()) {
-						out.print("Login");
-					}
+					req.getRequestDispatcher(LOGIN_VIEW);
 					break;
 				default:
-					res.setContentType("text/html");
-					try (PrintWriter out = res.getWriter()) {
-						out.print("NotFound");
-					}
+					req.getRequestDispatcher(NOTFOUND_VIEW);
+					break;
 			}
 		} catch (Exception e) {
-			// TODO: handle exception
+			logger.error("[Line 55]: " + e.getMessage());
 		}
 	}
 
@@ -55,51 +61,38 @@ public class AuthController extends HttpServlet {
 		String route = (req.getPathInfo() != null) ? req.getPathInfo().substring(1) : "";
 		try {
 			switch (route) {
-				case "signup":
-					AuthService.signUpInternal(data);
-					break;
 				case "login":
-					AuthService.loginInternal(data);
+					redirectPage = SIGNUP_VIEW;
+					LoginDTO loginDTO = HttpUtil.getBodyContentFromReq(req, LoginDTO.class);
+					AuthService.loginInternal(loginDTO);
+					req.getRequestDispatcher(redirectPage);
+					break;
+				case "signup":
+					redirectPage = LOGIN_VIEW;
+					SignUpDTO signUpDTO = HttpUtil.getBodyContentFromReq(req, SignUpDTO.class);
+					AuthService.signUpInternal(signUpDTO);
+					req.getRequestDispatcher(redirectPage);
 					break;
 				default:
-					res.setContentType("text/html");
-					try (PrintWriter out = res.getWriter()) {
-						out.print("NotFound");
-					}
+					redirectPage = NOTFOUND_VIEW;
+					break;
 			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
-
-	public static ResponseDTO<Object> signUp(SignUpDTO data) {
-		try {
-			return new ResponseDTO<>(ResponseStatus.OK, "Sign up successfully!", null);
-		} catch (SQLException e) {
-			return new ResponseDTO<>(ResponseStatus.INTERNAL_SERVER_ERROR,
-					"Server have some troubles, please comback again!", null);
+		} catch (AuthException | DataEmptyException | IllegalArgumentException e) {
+			logger.error("[Line 81]: " + e.getMessage());
+			res.getWriter().write(gson
+					.toJson(new ResponseDTO<>(ResponseStatus.BAD_REQUEST, e.getMessage(), null)));
 		} catch (NotFoundException e) {
-			return new ResponseDTO<>(ResponseStatus.NOT_FOUND, e.getMessage(), null);
-		} catch (DataEmptyException | AuthException | IllegalArgumentException | IOException
-				| ClassNotFoundException e) {
-			return new ResponseDTO<>(ResponseStatus.BAD_REQUEST, e.getMessage(), null);
+			logger.error("[Line 85]: " + e.getMessage());
+			res.getWriter().write(gson
+					.toJson(new ResponseDTO<>(ResponseStatus.NOT_FOUND, e.getMessage(), null)));
+		} catch (SQLException | TokenException | IOException | ClassNotFoundException e) {
+			logger.error("[Line 89]: " + e.getMessage());
+			res.getWriter().write(gson
+					.toJson(new ResponseDTO<>(ResponseStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
+							null)));
+		} finally {
+			req.getRequestDispatcher(redirectPage).forward(req, res);
 		}
-	}
-
-	public static ResponseDTO<Object> login(LoginDTO data) {
-		try {
-
-			return new ResponseDTO<>(ResponseStatus.OK, "Login successfully!", null);
-		} catch (AuthException e) {
-			return new ResponseDTO<>(ResponseStatus.BAD_REQUEST, e.getMessage(), null);
-		} catch (NotFoundException e) {
-			return new ResponseDTO<>(ResponseStatus.NOT_FOUND, e.getMessage(), null);
-		} catch (SQLException e) {
-			return new ResponseDTO<>(ResponseStatus.INTERNAL_SERVER_ERROR, e.getMessage(), null);
-		} catch (TokenException | IOException | ClassNotFoundException e) {
-			return new ResponseDTO<>(ResponseStatus.INTERNAL_SERVER_ERROR, e.getMessage(), null);
-		}
-
 	}
 
 	public static ResponseDTO<Boolean> checkAccessToken() {
