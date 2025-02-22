8f1ec9ad7fec30740e08987042550beb17a9b2de
LeviathanST <phamquochung170505@gmail.com>
2025-02-22 21:07:02 +0700

    update(count_mistake): change how to check

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -115,21 +115,4 @@ CREATE TABLE IF NOT EXISTS event (
 
     FOREIGN KEY (generation_id) REFERENCES generation(id)
 );
--- Trigger to check count_mistake
-DELIMITER //
-
-CREATE TRIGGER before_update_account
-BEFORE UPDATE ON user_account
-FOR EACH ROW
-BEGIN 
-	IF NEW.count_mistake != OLD.count_mistake THEN
-        IF NEW.count_mistake < 0 OR NEW.count_mistake > 3 THEN
-            SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = "Mistake counting must be between 0 and 3!!!";
-        END IF;
-    END IF;
-END;
-//
-
-DELIMITER ;
 
